2e730bfc908ef6a912205d1fa273daca51615cbf
Palermo Alien <palermoriley23@gmail.com>
2024-02-15 16:13:01 -0600

    progress on 1-9

--- add_two_numbers.py
+++
@@ -1,4 +0,0 @@
-Var_birthmonth = 12
-Var_birthday = 30
-print("birth month + birthday=")
-print(Var_birthmonth+Var_birthday)
--- print_hello_world.py
+++
@@ -1,4 +0,0 @@
-Var_name = 'Riley'
-name = Var_name
-hello_message = "Hi I am "+Var_name
-print(hello_message)
