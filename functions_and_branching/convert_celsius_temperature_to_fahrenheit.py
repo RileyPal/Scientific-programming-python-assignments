f52a9568c3a7f80c977ef94164e179e9b3b18a60
Riley <palermoriley23@gmail.com>
2024-02-29 15:48:40 -0600

    progress on 1-9

---
+++ functions_and_branching/convert_celsius_temperature_to_fahrenheit.py
@@ -0,0 +1,8 @@
+user_input: str = input("Celsius or Fahrenheit")
+system: str = user_input
+if system == "f" or "fahrenheit" or "F" or "Fahrenheit":
+    return i=1
+user_input: int = input("What temp")
+n: int = user_input
+ctof = (5/9)*n+32
+ftoc = (9/5)*n+32
