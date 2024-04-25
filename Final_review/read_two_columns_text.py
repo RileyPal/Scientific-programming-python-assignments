bd281cc305732e4326d23086cb2a57fdd849b1dc
Riley <palermoriley23@gmail.com>
2024-04-25 15:12:57 -0500

    progress review stuff

---
+++ Final_review/read_two_columns_text.py
@@ -0,0 +1,22 @@
+"""
+Author: Riley Palermo
+"""
+
+import numpy as np
+
+def read_two_columns_text(filename: str) -> np.ndarray:
+    try:
+        data = np.loadtxt(filename, dtype=float).T
+        if data.shape[0] != 2:
+            raise ValueError("File should contain exactly two columns")
+        return data
+    except OSError:
+        raise OSError("File not found for reading")
+
+if __name__ == "__main__":
+    filename = "volumes_energies.dat"
+    try:
+        data = read_two_columns_text(filename)
+        print(f"{data=}, shape={data.shape}")
+    except OSError as e:
+        print(e)
